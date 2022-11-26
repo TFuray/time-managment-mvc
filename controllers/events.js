a453fb9f59f2e128d8fe5380f58c9194760fd971
TFuray <pk.master881@gmail.com>
2022-11-26 12:29:12 -0800

    adding event view

--- controllers/events.js
+++ controllers/events.js
@@ -3,10 +3,11 @@ const Event = require('../models/Event')
 module.exports = {
     getEvents: async (req, res)=>{
         try {
-            const eventItems = await Todo.find()
-            
+            const eventItems = await Event.find()
+            res.render('events.ejs', {events: eventItems}) 
         } catch (err) {
-            
+           console.log(err) 
         }
-    }
+    },
+    
 }
